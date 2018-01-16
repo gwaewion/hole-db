fe96bcbca9cac130b222e60200b35969664a27b9
gwaewion <gwaewion@gmail.com>
2018-01-16 11:48:25 +0300

    init commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z $(ls -A ${DB_DATA_PATH}) ]
+then
+	echo "=> Installing DB"
+	mysql_install_db --user=mysql --datadir="${DB_DATA_PATH}"
+	sleep 3
+	echo "=> Starting DB for set DB root password"
+	mysqld --user=mysql --datadir="${DB_DATA_PATH}" &
+	sleep 3
+	echo "=> Setting DB root password"
+	mysqladmin -u root password "${DB_ROOT_PASSWORD}"
+	echo "=> Enable remote access as root from any host"
+	#echo "update user set host=’%’ where user=’root’ and host=’ubuntuserv’;\nflush privileges;" > /tmp/sql
+	mysql --user=root --password="${DB_ROOT_PASSWORD}" mysql -e "update user set host='%' where user='root' and host='localhost';"
+	mysql --user=root --password="${DB_ROOT_PASSWORD}" mysql -e "flush privileges;"
+	echo "=> Killing all mysqld instances"
+	killall mysqld
+	echo "=> Starting DB after installation"
+	mysqld --user=mysql --datadir="${DB_DATA_PATH}"
+else
+	echo "=> Starting DB"
+	mysqld --user=mysql --datadir="${DB_DATA_PATH}"
+fi
+
